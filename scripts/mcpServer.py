8a7cbbb043988977c26e5626312d238a329b49f6
myoun8 <minjae.youn7718@gmail.com>
2026-07-06 16:58:51 -0400

    fix

--- scripts/mcpServer.py
+++ scripts/mcpServer.py
@@ -91,10 +91,7 @@ def list_datasources() -> list:
 
 @mcp.tool()
 def list_data_files(source: str = "ncnr", pathlist: list[str] | None = None) -> dict:
-    """Browse files/subdirs at a path within a reductus data source.
-       Use this for any requests of finding intent of a file. If 
-       pathlist is omitted from a request of the intent of a file,
-       ask user for pathlist"""
+    """Browse files/subdirs at a path within a reductus data source."""
     return reductus_api.get_file_metadata(source=source, pathlist=pathlist or [])
 
 
